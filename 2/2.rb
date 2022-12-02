e4a28de5fbe99a47babcce3ace0ebe10c17a84ed
Max Pekarsky <pekarsky.maxim@gmail.com>
2022-12-02 11:21:35 -0500

    add day2

---
+++ 2/2.rb
@@ -0,0 +1,34 @@
+require 'pry'
+
+class CalorieCounter
+  def initialize(file_name)
+    elf = File.read(file_name).split("\n\n")
+    @input_array = elf.map do |line|
+      line.split("\n").map {|num| num.to_i }
+    end
+  end
+
+  def get_max_group_calories
+    max = 0;
+
+    @input_array.each do |elf_foods|
+      total_calories = elf_foods.sum
+      max = [max, total_calories].max
+    end
+
+    max
+  end
+
+  def get_top_three_group_calories
+    calories_per_elf = []
+
+    @input_array.each do |elf_foods|
+      calories_per_elf << elf_foods.sum
+    end
+
+    calories_per_elf.sort.last(3).sum
+  end
+end
+
+counter = CalorieCounter.new('input.txt')
+counter.get_top_three_group_calories

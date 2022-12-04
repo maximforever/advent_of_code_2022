826564968a8ea6f135677fdac175f3a81885c35c
Max Pekarsky <pekarsky.maxim@gmail.com>
2022-12-04 11:04:18 -0500

    add day 4

---
+++ 4/4.rb
@@ -0,0 +1,14 @@
+require 'pry'
+
+class ClassName
+  def initialize(file_name)
+    @rounds = File.read(file_name).split("\n").map {|line| line.split(" ")}
+  end
+
+  def method
+    total_score = 0
+  end
+end
+
+counter = ClassName.new('input.txt')
+counter.method
